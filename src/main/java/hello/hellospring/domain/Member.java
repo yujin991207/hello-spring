a504344428947f312344dd07b829c831364fc42b
yujin991207 <yujin991207@naver.com>
2023-07-23 15:15:37 +0900

    JPA 강의까지 완료!

--- src/main/java/hello/hellospring/domain/Member.java
+++ src/main/java/hello/hellospring/domain/Member.java
@@ -1,7 +1,6 @@
 package hello.hellospring.domain;
 
 public class Member {
-
     private Long id; // 시스템 저장하는 id
     private String name;
 
@@ -12,11 +11,9 @@ public class Member {
     public void setId(Long id) {
         this.id = id;
     }
-
     public String getName() {
         return name;
     }
-
     public void setName(String name) {
         this.name = name;
     }
